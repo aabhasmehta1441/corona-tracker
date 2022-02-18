dc6faee7aa5707c72e3e529bd14af6fa6fefdaac
aabhas mehta <mehtaaab@sheridancollege.ca>
2022-02-18 14:24:52 -0500

    test

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
         <nav class="navbar navbar-expand-lg custom_nav-container ">
           <a class="navbar-brand" href="index.html">
             <span>
-              Orthoc
+              DIVINE DENTAL
             </span>
           </a>
 
